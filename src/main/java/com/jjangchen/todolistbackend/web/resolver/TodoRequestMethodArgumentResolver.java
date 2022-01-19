16c18500f8cf07c24ffa0cfde6fb97ec747e5ca0
Jiho <dlwlgh9412@gmail.com>
2022-01-19 20:14:40 +0900

    custom argumentResolver 클래스 임시생성

--- src/main/java/com/jjangchen/todolistbackend/web/resolver/TodoRequestMethodArgumentResolver.java
+++ src/main/java/com/jjangchen/todolistbackend/web/resolver/TodoRequestMethodArgumentResolver.java
@@ -1,21 +1,32 @@
 package com.jjangchen.todolistbackend.web.resolver;
 
+import com.jjangchen.todolistbackend.entity.TodoAccount;
 import com.jjangchen.todolistbackend.web.dto.TodoRequestDto;
+import com.jjangchen.todolistbackend.web.service.TodoAccountService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.core.MethodParameter;
 import org.springframework.web.bind.support.WebDataBinderFactory;
 import org.springframework.web.context.request.NativeWebRequest;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.ModelAndViewContainer;
 
-public class TodoArgumentResolver implements HandlerMethodArgumentResolver {
+@RequiredArgsConstructor
+public class TodoRequestMethodArgumentResolver implements HandlerMethodArgumentResolver {
+    private final TodoAccountService accountService;
 
     @Override
     public boolean supportsParameter(MethodParameter parameter) {
-        return TodoRequestDto.class.isAssignableFrom(parameter.getParameterType());
+        return parameter.getParameterType() == TodoRequestDto.class;
     }
 
     @Override
     public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
-        return null;
+        TodoRequestDto dto = (TodoRequestDto) parameter;
+        dto.setUser(loadAccount());
+        return dto;
+    }
+
+    private TodoAccount loadAccount() {
+        return accountService.loadAccountByContext();
     }
 }
