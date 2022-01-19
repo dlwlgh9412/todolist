b6b53ca9e0b8ba2c3c81511212ece78e3b5a40c3
Jiho <dlwlgh9412@gmail.com>
2022-01-19 20:15:32 +0900

    argumentresolver 설정추가

--- src/main/java/com/jjangchen/todolistbackend/web/config/WebConfig.java
+++ src/main/java/com/jjangchen/todolistbackend/web/config/WebConfig.java
@@ -2,8 +2,10 @@ package com.jjangchen.todolistbackend.web.config;
 
 import com.jjangchen.todolistbackend.repository.TodoAccountRepository;
 import com.jjangchen.todolistbackend.web.interceptor.AttachInterceptor;
+import com.jjangchen.todolistbackend.web.resolver.TodoRequestMethodArgumentResolver;
 import com.jjangchen.todolistbackend.web.service.TodoAccountService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -27,5 +29,10 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(attachInterceptor).addPathPatterns("/todos/*");
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(new TodoRequestMethodArgumentResolver(todoAccountService));
     }
 }
