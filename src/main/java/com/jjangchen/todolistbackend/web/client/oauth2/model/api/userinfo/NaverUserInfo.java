a45b0623a1ef4b3c4ac4509b1d8343ff875b54a9
dlwlgh9412 <dlwlgh9412@gmail.com>
2022-01-26 15:27:33 +0900

    카카오 계정 정보 DTO추가

---
+++ src/main/java/com/jjangchen/todolistbackend/web/client/oauth2/model/api/userinfo/NaverUserInfo.java
@@ -0,0 +1,4 @@
+package com.jjangchen.todolistbackend.web.client.oauth2.model.api.userinfo;
+
+public class NaverUserInfo implements TodoOauth2UserInfo {
+}
---
+++ src/main/java/com/jjangchen/todolistbackend/web/client/oauth2/model/api/userinfo/TodoOauth2AccountInfo.java
@@ -0,0 +1,4 @@
+package com.jjangchen.todolistbackend.web.client.oauth2.model.api.userinfo;
+
+public interface TodoOauth2AccountInfo {
+}
